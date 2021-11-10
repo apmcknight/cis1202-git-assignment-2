28eabe700bf1eb9045cd16c79bc335e413219d69
Adam McKnight <adammcknight@me.com>
2021-11-10 14:05:34 -0500

    Fixed the errors. Moved cin statements to new lines for better read-ability. Defined the ints n1-n3. Tested the program. Averages tested are correct.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -9,11 +9,20 @@ float average(int i1, int i2, int i3)
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0;
+	int n2 = 0;
+	int n3 = 0;
+
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	
+	cout << "First number: "; 
+	cin >> n1;
+	
+	cout << "Second number: "; 
+	cin >> n2;
+
+	cout << "Third number: "; 
+	cin >> n2;
 
 	float a = average(n1, n2, n3);
 
